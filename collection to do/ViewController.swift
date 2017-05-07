dc3c66fd483a060fbfddcd8ca26f1102ff2b0bc8
樋口大樹 <daikiit27@gmail.com>
2017-05-07 15:44:48 +0900

    Initial Commit

---
+++ collection to do/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  collection to do
+//
+//  Created by 樋口大樹 on 2017/05/07.
+//  Copyright © 2017年 樋口大樹. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
