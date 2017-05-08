bc5400377a098e8326db056144c82e9b4dc1af93
樋口大樹 <daikiit27@gmail.com>
2017-05-08 13:35:51 +0900

    aa

--- collection to do/ListCollectionViewController.swift
+++ collection to do/ListCollectionViewController.swift
@@ -79,12 +79,12 @@ class ListCollectionViewController: UICollectionViewController {
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 0
     }
-
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-    
-        return cell
-    }
+//
+//    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+//    
+//        return cell
+//    }
 
 
 }
